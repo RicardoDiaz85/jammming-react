d0ef870bcf573356a6717472d3746a504876ab7c
Ricado Diaz <ricardo_diaz85@hotmail.com>
2024-11-28 11:46:09 -0500

    Created Spinner component.
     Added Spinner component and styled it in the Components directory.

---
+++ src/Components/Spinner/Spinner.js
@@ -0,0 +1,12 @@
+function LoadingSpinner() {
+
+    return (
+        <div className="loading-spinner">
+            <div className="spinner"></div>
+            <p>Saving your playlist...</p>
+        </div>
+    );
+
+}
+
+export default LoadingSpinner;
