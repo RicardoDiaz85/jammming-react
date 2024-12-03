b140de8bd7943f4ea50ab9b5c0824ab0065b03f8
Ricado Diaz <ricardo_diaz85@hotmail.com>
2024-12-03 12:44:23 -0500

    Update SpotifyAuth.js

--- src/utils/SpotifyAuth.js
+++ src/utils/SpotifyAuth.js
@@ -1,7 +1,7 @@
 let accessToken; // Declare accessToken at a higher scope so it persists
 
 const clientId = 'aad34981898a496ebb6c3889e5644736'; // Replace with your actual Client ID
-const redirectUri = 'http://localhost:3000'; // Your redirect URIc
+const redirectUri = 'http://localhost:3000'; // Your redirect URI
 const scopes = [
     'playlist-modify-public',
     'playlist-modify-private',
